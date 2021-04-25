023fd035bd75e9718ec55b52acfff94968b7a480
Kamel <kamel.bencherif@epitech.eu>
2021-04-25 19:18:51 +0200

    Fix Help.

--- sources/functions/help.c
+++ sources/functions/help.c
@@ -9,7 +9,7 @@
 
 void help_commands(char *value, clients_data_t *client, server_t *server)
 {
-    client->msg = "USER, PASS, CWD, CDUP, QUIT, PORT, PASV, STOR, RETR, LIST,\n\
-                   DELE, PWD, HELP, NOOP\n";
+    client->msg = "USER, PASS, CWD, CDUP, QUIT, PORT, PASV, STOR, RETR, LIST,\
+                   DELE, PWD, HELP, NOOP\r\n";
     client->return_code = HELP_OK;
 }
